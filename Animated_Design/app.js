41ab04014f74f1e17641ecc443b0ef8c748b0cc1
Kshitiz Duwadi <111699407+drskshi@users.noreply.github.com>
2022-11-07 08:24:14 +0545

    Add files via upload
    Animated Login Design Form

---
+++ Animated_Design/app.js
@@ -0,0 +1,25 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+
+const sign_up_btn = document.querySelector("#sign-up-btn");
+
+const container = document.querySelector(".container");
+
+
+
+
+sign_up_btn.addEventListener("click", () => {
+
+  container.classList.add("sign-up-mode");
+});   
+
+
+
+
+sign_in_btn.addEventListener("click", () => {
+
+  container.classList.remove("sign-up-mode");
+
+});
+("https://www.tiktok.com/foryou?is_copy_url=1&is_from_webapp=v1&item_id=7121307075388460315#/@ishani__055/video/7121307075388460315")
+
+
